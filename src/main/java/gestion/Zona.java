c58952b612cbd5d861c8f6757847b5da8ede9c39
otaku9820 <aguido@unal.edu.co>
2023-09-25 22:07:45 -0500

    Taller java 5

---
+++ src/main/java/gestion/Zona.java
@@ -0,0 +1,47 @@
+package gestion;
+
+import zooAnimales.Animal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Zona {
+    private String nombre;
+    private Zoologico zoo;
+    private List<Animal> animales ;
+
+    public Zona(String nombre, Zoologico zoo) {
+        this.nombre=nombre;
+        this.zoo=zoo;
+    }
+
+    public Zona() {
+        this(null,null)
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre=nombre;
+    }
+
+    public String getNombre() {
+        return (this.nombre);
+    }
+
+    public void setZoo(Zoologico zoo) {
+        this.zoo=zoo;
+    }
+
+    public Zoologico getZoo() {
+        return (this.zoo);
+    }
+
+    public void agregarAnimales(Animal animal) {
+        this.animales.add(animal);
+        animal.agregarZona(this);
+    }
+
+    public int cantidadAnimales() {
+        return (this.animales.size());
+    }
+
+}
---
+++ src/main/java/gestion/Zoologico.java
@@ -0,0 +1,53 @@
+package gestion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Zoologico {
+    private String nombre;
+    private String ubicacion;
+    private List<Zona> zonas;
+
+    public Zoologico(String nombre, String ubicacion) {
+        this.nombre=nombre;
+        this.ubicacion=ubicacion;
+        this.zonas=new List<Zona>();
+    }
+    public Zoologico() {
+        this.nombre=null;
+        this.ubicacion=null;
+        this.zonas = new ArrayList<>();
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre=nombre;
+    }
+
+    public String getNombre() {
+        return (this.nombre);
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion=ubicacion;
+    }
+
+    public String getUbicacion() {
+        return (this.ubicacion);
+    }
+
+    public void agregarZonas(Zona zona) {
+        this.zonas.add(zona);
+    }
+
+    public int cantidadTotalAnimales() {
+        int Total=0;
+        for (Zona i : this.zonas) {
+            int x= i.cantidadAnimales();
+            Total= Total + x;
+        }
+        return (Total);
+    }
+    public List<Zona> getZona() {
+        return zonas;
+    }
+}
