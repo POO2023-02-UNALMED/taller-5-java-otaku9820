38a825baf976493048d91c0ef6e706815ea4fc8f
otaku9820 <aguido@unal.edu.co>
2023-09-26 05:49:52 -0500

    Update Animal.java

--- src/test/java/zooAnimales/Animal.java
+++ src/test/java/zooAnimales/Animal.java
@@ -1,95 +1,56 @@
 package zooAnimales;
-
-import gestion.Zona;
-
-
 import java.util.ArrayList;
-import java.util.List;
 
-public class Animal {
-	private static int totalAnimales;
-	private String nombre;
-	private int edad;
-	private String habitat;
-	private String genero;
-	private List<Zona> zona = new ArrayList<>();
+public class Ave extends Animal{
+	private static ArrayList<Ave> listado = new ArrayList<Ave>();
+	public static int halcones;
+	public static int aguilas;
+	private String colorPlumas;
 
-	public Animal(String nombre, int edad, String habitat, String genero) {
-		totalAnimales ++;
-		this.nombre=nombre;
-		this.edad=edad;
-		this.habitat=habitat;
-		this.genero=genero;
+	public Ave(String nombre,int edad,String habitat,String genero,String colorPlumas){
+		super(nombre,edad,habitat,genero);
+		this.colorPlumas = colorPlumas;
+		listado.add(this);
 	}
 
-	public Animal() {
-		totalAnimales ++;
-		this.nombre=null;
-		this.edad=0;
-		this.habitat=null;
-		this.genero=null;
+	public Ave(){
+		listado.add(this);
 	}
 
-	public void setNombre(String nombre) {
-		this.nombre=nombre;
+	public static int cantidadAves(){
+		return listado.size();
 	}
 
-	public String getNombre() {
-		return (this.nombre);
+	@Override
+	public String movimiento(){
+		return "volar";
 	}
 
-	public void setEdad(int edad) {
-		this.edad=edad;
+	public static Ave crearHalcon(String nombre,int edad,String genero){
+		Ave halcon = new Ave(nombre,edad,"montanas",genero,"cafe glorioso");
+		halcones += 1;
+		return halcon;
 	}
 
-	public int getEdad() {
-		return (this.edad);
+	public static Ave crearAguila(String nombre,int edad,String genero){
+		Ave aguila = new Ave(nombre,edad,"montanas",genero,"blanco y amarillo");
+		aguilas += 1;
+		return aguila;
 	}
 
-	public void setHabitat(String habitat) {
-		this.habitat=habitat;
+	public static ArrayList<Ave> getListado(){
+		return listado;
 	}
 
-	public String getHabitat() {
-		return (this.habitat);
+	public static void setListado(ArrayList<Ave> listado){
+		Ave.listado = listado;
 	}
 
-	public void setGenero(String genero) {
-		this.genero=genero;
+	public String getColorPlumas(){
+		return this.colorPlumas;
 	}
 
-	public String getGenero() {
-		return (this.genero);
-	}
-
-	public void agregarZona(Zona zona) {
-		this.zona.add(zona);
-	}
-
-	public static String totalPorTipo() {
-		return("Mamiferos: " + (Mamifero.cantidadMamiferos()) + "\n" + "Aves: " + (Ave.cantidadAves())) + "\n" + "Reptiles: " + (Reptil.cantidadReptiles())+"\n" +"Peces: " + (Pez.cantidadPeces())+"\n"+ "Anfibios: " + (Anfibio.cantidadAnfibios());
-	}
-
-
-	public String toString() {
-		if (this.zona.size()<1) {
-			System.out.println("Mi nombre es " + this.getNombre() + ", tengo una edad de " + this.getEdad() + ", habito en " + this.getHabitat() + "y mi genero es " + this.getGenero());
-			return("Mi nombre es " + this.getNombre() + ", tengo una edad de " + this.getEdad() + ", habito en " + this.getHabitat() + " y mi genero es " + this.getGenero());
-		}
-		else {
-			String nombreZona=null;
-			String nombreZoo=null;
-			for(Zona zona:this.zona) {
-				nombreZona=zona.getNombre();
-				Zoologico zoo=zona.getZoo();
-				nombreZoo= zoo.getNombre();
-			}
-			System.out.println("Mi nombre es " + this.getNombre() + ", tengo una edad de " + this.getEdad() + ", habito en " + this.getHabitat() + " y mi genero es " + this.getGenero() + " la zona en la que me ubico es " + nombreZona + ", en el " + nombreZoo);
-			return ("Mi nombre es " + this.getNombre() + ", tengo una edad de " + this.getEdad() + ", habito en " + this.getHabitat() + " y mi genero es " + this.getGenero() + " la zona en la que me ubico es " + nombreZona + ", en el " + nombreZoo);
-		}
-	}
-
-	public String movimiento() {
-		return ("desplazarse");
+	public void setColorPlumas(String colorPlumas){
+		this.colorPlumas = colorPlumas;
 	}
 }
